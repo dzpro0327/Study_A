55f6f0f52ab9107e2bdc678ef2bce5f63f8b727b
dongjoo <dzpro0327@gmail.com>
2020-12-23 08:10:14 +0900

    12/23

---
+++ JavaTPC/src/TPC02.java
@@ -0,0 +1,27 @@
+public class TPC02 {
+    public static void main(String[] args) {
+        // 프로그래밍의 3대요소 : 변수, 자료형(data type), 할당(=)
+
+        int a, b, c;
+        a = 1;
+        b = 1;
+        c = a + b; // c = 2
+
+        System.out.println(c);
+
+        float f;
+        f = 34.5f;
+        System.out.println(f);
+
+        char d;
+        d = 'A';
+        System.out.println(d);
+
+        boolean g;
+        g = true;
+        System.out.println(g);
+
+
+
+    }
+}
