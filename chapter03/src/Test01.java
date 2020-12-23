0b94fade8916c3ae35fd0333bcbf8ca632921e39
dongjoo <dzpro0327@gmail.com>
2020-12-24 01:33:49 +0900

    12/24

--- chapter03/src/Test01.java
+++ chapter03/src/Test01.java
@@ -1,7 +1,9 @@
 public class Test01 {
     public static void main(String[] args) {
         int fahrenheit = 100;
-        float celcius = (float) (0.5556 * (fahrenheit-32));
+        float celcius = (int)((5/9f) * (fahrenheit-32) * 100 + 0.5) / 100f;
+        float celcius2 = (5/9f) * (fahrenheit-32);
         System.out.println(celcius);
+        System.out.printf("%.2f" ,celcius2);
     }
 }
