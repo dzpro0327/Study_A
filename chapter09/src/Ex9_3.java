644c108a53d3aeb2b61ddb4c7141179d96309d4d
dongjoo <dzpro0327@gmail.com>
2020-12-30 09:43:30 +0900

    12/30

---
+++ chapter09/src/Ex9_3.java
@@ -0,0 +1,12 @@
+public class Ex9_3 {
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+
+        System.out.println(str1.equals(str2));  // 인스턴스 주소값은 다르고 값은 같다..
+        System.out.println(str1.hashCode());  //
+        System.out.println(str2.hashCode());
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+    }
+}
